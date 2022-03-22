8e91fe4b6dc41ed225f588af0de6a2ce0c6b1416
nizzola <marcio@nizzola.com.br>
2022-03-22 18:33:35 -0300

    implement validation

--- projeto1/src/app/cadastro/cadastro.module.ts
+++ projeto1/src/app/cadastro/cadastro.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -8,10 +8,12 @@ import { CadastroPageRoutingModule } from './cadastro-routing.module';
 
 import { CadastroPage } from './cadastro.page';
 
+
+
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    FormsModule, ReactiveFormsModule,
     IonicModule,
     CadastroPageRoutingModule
   ],
--- projeto1/src/app/cadastro/cadastro.page.ts
+++ projeto1/src/app/cadastro/cadastro.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Produtos } from '../models/produtos';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,12 +10,23 @@ import { Produtos } from '../models/produtos';
 export class CadastroPage implements OnInit {
 
   produto: Produtos = new Produtos();
+  formCadastro: FormGroup;
 
-  constructor() {
-    
+  constructor(private formBuilder: FormBuilder ) {
+      this.formCadastro = this.formBuilder.group({
+        descricao: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+        venda: ['0', Validators.required],
+        custo: ['0', Validators.required],
+        estoque: ['0', Validators.required],
+        unidade: ['', Validators.required],
+        ativo: ['', Validators.required]
+      });
    }
 
   ngOnInit() {
   }
-
+  
+  SalvarCadastro() {
+    console.log('Form submetido: ', this.formCadastro.valid);
+  }
 }
